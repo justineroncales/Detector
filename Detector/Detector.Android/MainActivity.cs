72ed2888f5ffde8627b73c3c9ee01172f51aa8ff
inutil <50046454+justineroncales@users.noreply.github.com>
2022-10-22 17:10:43 +0800

    Updates

--- Detector/Detector.Android/MainActivity.cs
+++ Detector/Detector.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Plugin.CurrentActivity;
 
 namespace Detector.Droid
 {
-    [Activity(Label = "Detector", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Noah", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
